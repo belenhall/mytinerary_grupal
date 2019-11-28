cca8ad71879af2cf34edf69e13422924ed01bc07
Hall <belen.hall@accenture.com>
2019-11-28 15:55:05 -0300

    facilitarte la vida

--- routes.js
+++ routes.js
@@ -7,56 +7,17 @@ const user = require("./user.js");
 const bcrypt = require("bcrypt");
 const db = require("./database");
 
-module.exports /*router.get("/api/cities", async (req, res) => {
-  var ciudades = await city.find(function(err, results) {
-    if (err) return console.error(err);
-    console.log("joya", results);
-  });
-  res.json(ciudades);
-});
-router.get("/api/itineraries", async (req, res) => {
-  var itineraries = await itinerary.find(function(err, results) {
-    if (err) return console.error(err);
-    console.log("joya", results);
-  });
-  res.json(itineraries);
-});
-*/ =
-  /*router.get("/api/itineraries/activities", async (req, res) => {
-  var itineraries = await itinerary.find(function(err, results) {
-    if (err) return console.error(err);
-    console.log("joya", results);
-  });
-  res.json(itineraries.map(item => item.activities).flat());
-});
-*/
+module.exports  =
+
   router.get("/api/activities", async (req, res) => {
     itinerary
       .find({ activities: /[^b]/ }, { _id: 0, activities: 1 })
       .then(activities => res.json(activities));
   });
 
-/*router.get("/api/users", async (req, res) => {
-  var users = await user.find(function(err, results) {
-    if (err) return console.error(err);
-    console.log("joya", results);
-  });
-  res.json(users);
-});*/
 
-/*app.post('/user/create', function (req, res) {
-  bcrypt.hash(req.body.passwordsignup, saltRounds, function (err,   hash) {
- db.User.create({
-   name: req.body.usernamesignup,
-   email: req.body.emailsignup,
-   password: hash
-   }).then(function(data) {
-    if (data) {
-    res.redirect('/home');
-    }
-  });
- });
-});*/
+
+
 const userRegisterUri = "/api/users/register";
 const userLoginUri = "/api/users/login";
 const userUri = "/api/users";
@@ -94,9 +55,7 @@ const routesAndModels = [
   [itinerariesRoute, itinerary],
   [userUri, user]
 ];
-//genericGetAll([citiesRoute, itinerariesRoute], [city, itinerary])
-/*genericGetAll(itinerariesRoute, itinerary)
-genericGetAll(citiesRoute, city)*/
+
 routesAndModels.map(subarray => genericGetAll(subarray[0], subarray[1]));
 
 router.post("/api/itineraries", (req, res) => {
@@ -119,11 +78,7 @@ router.get("/api/cities/:name", async (req, res) => {
     .then(ciudad => res.json(ciudad))
     .catch(err => console.log(err));
 
-  /*{
-          if(err) return console.error(err);
-          console.log("joya",results)
-      })
-      res.json(ciudades)*/
+ 
 });
 router.get("/api/itineraries/:city", async (req, res) => {
   let itinerarioBuscado = req.params.city;
@@ -188,15 +143,6 @@ router.get("/api/activities/byName/:name", async (req, res) => {
     .catch(err => console.log(err));
 });
 
-/*router.put("api/itineraries/byTitle/:title/activities", async (req, res, value) => {
-  let itinerarioBuscado = req.params.title;
- itinerary
- .update({title: itinerarioBuscado},{$addToSet: {activities: value} })
- .then(itinerary => res.send("itinerario actualizado"))
- .catch(err => console.log(err))
-  
-});
-*/
 
 router.put("api/itineraries", async (req, res, value) => {
   res.send("itinerario actualizado");
@@ -208,72 +154,4 @@ router.put("/api/itineraries/byTitle/:title", function(req, res) {
   res.send(`${itineraryByTitle} updated`);
 });
 
-/*router.put('/api/itineraries/byTitle/:title', function (req, res) {
-  let itineraryByTitle = req.params.title;
-  itinerary
- .findOne({title: itineraryByTitle}, (err, doc) => {
-   doc.rating.$inc();
-   doc.save()
- })
-})*/
 
-/*Model.findOne({ name: 'bourne' }, function (err, doc){
-  doc.name = 'jason bourne';
-  doc.visits.$inc();
-  doc.save();
-});*/
-
-//$addToSet
-//$set
-/* 
-  //this is code that you have already implemented in your project
-  router.get('/all',
-  (req, res) => {
-      cityModel.find({})
-          .then(files => {
-              res.send(files)
-          })
-          .catch(err => console.log(err));
-  });
-  
-  //this is how you implement a city route by specific city
-  router.get('/:name',
-  (req, res) => {
-        let cityRequested = req.params.name;
-        cityModel.findOne({ name: cityRequested })
-          .then(city => {
-              res.send(city)
-          })
-          .catch(err => console.log(err));
-  */
-//API CALLS
-
-/*app.get("/", (req, res) => {
-    res.send({ express: "Vamo espres" });
-  });
-  
-  app.get("/test", (req, res) => {
-    res.send({ express: "Vamo test" });
-  });
-  
-  app.post("/api/world", (req, res) => {
-    console.log(req.body);
-    res.send(`Hola soy express${req.body.post}`);
-  });
-  
-  router.use(function(req, res, next) {
-    // log each request to the console
-    console.log(req.method, req.url);
-  
-    // continue doing what we were doing and go to the route
-    next();
-  });
-  
-  router.get("/", function(req, res) {
-    res.send("Im the home page");
-  });
-  
-  router.get("/about", function(req, res) {
-    res.send("im the about page!");
-  });
-*/
